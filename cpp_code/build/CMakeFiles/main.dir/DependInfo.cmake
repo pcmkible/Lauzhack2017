3d0ee04eb8bec10d189bf2a0e479fe059d95b2bc
aleksa <stankovic.s.aleksa@gmail.com>
2017-11-12 04:21:03 +0100

    Updated ideas

--- cpp_code/build/CMakeFiles/main.dir/DependInfo.cmake
+++ cpp_code/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/aleksa/lauzhack/main.cpp" "/home/aleksa/lauzhack/build/CMakeFiles/main.dir/main.cpp.o"
+  "/home/aleksa/Lauzhack2017/cpp_code/main.cpp" "/home/aleksa/Lauzhack2017/cpp_code/build/CMakeFiles/main.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- cpp_code/build/cmake_install.cmake
+++ cpp_code/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/aleksa/lauzhack
+# Install script for directory: /home/aleksa/Lauzhack2017/cpp_code
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/aleksa/lauzhack/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/aleksa/Lauzhack2017/cpp_code/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
